520d69bfae7501a09cd4f8faeb30b6bebf05cb42
JaaJPlayz <hiagoaugustofcunha@gmail.com>
2024-06-14 12:18:27 -0300

    Feat: Made the task 3

--- projects/models.py
+++ projects/models.py
@@ -33,3 +33,49 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=50, null=False, blank=False)
+    description = models.TextField(null=False, blank=False, max_length=500)
+    github_url = models.URLField(null=False, blank=False, max_length=500)
+    keyword = models.CharField(max_length=50, null=False, blank=False)
+    key_skill = models.CharField(max_length=50, null=False, blank=False)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+
+    def get_projects(self):
+        return Project.objects.all()
+
+    def get_project_by_id(self, id):
+        return Project.objects.get(id=id)
+
+    def create_project(
+        self, name, description, github_url, keyword, key_skill, profile
+    ):
+        return Project.objects.create(
+            name=name,
+            description=description,
+            github_url=github_url,
+            keyword=keyword,
+            key_skill=key_skill,
+            profile=profile,
+        )
+
+    def update_project(
+        self, id, name, description, github_url, keyword, key_skill, profile
+    ):
+        project = Project.objects.get(id=id)
+        project.name = name
+        project.description = description
+        project.github_url = github_url
+        project.keyword = keyword
+        project.key_skill = key_skill
+        project.profile = profile
+        project.save()
+
+    def delete_project(self, id):
+        project = Project.objects.get(id=id)
+        project.delete()
+
+    def __str__(self):
+        return self.name
--- super_portfolio/urls.py
+++ super_portfolio/urls.py
@@ -24,10 +24,11 @@ from rest_framework_simplejwt.views import (
     TokenVerifyView,
 )
 
-from projects.views import ProfileViewSet
+from projects.views import ProfileViewSet, ProjectViewSet
 
 router = routers.DefaultRouter()
 router.register(r"profiles", ProfileViewSet)
+router.register(r"projects", ProjectViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
