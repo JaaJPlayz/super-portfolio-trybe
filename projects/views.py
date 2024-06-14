6f668be47541067047a6ca2b586af7815cd74150
JaaJPlayz <hiagoaugustofcunha@gmail.com>
2024-06-14 12:46:20 -0300

    Feat: Made the 4th task

--- projects/views.py
+++ projects/views.py
@@ -1,10 +1,37 @@
+from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
 from .models import Profile, Project
 from .serializers import ProfileSerializer, ProjectSerializer
 
 
 class ProfileViewSet(viewsets.ModelViewSet):
+    def get_permissions(self):
+        if self.request.method == "GET":
+            return [AllowAny()]
+        return [IsAuthenticated()]
+
+    def retrieve(self, request, *args, **kwargs):
+        if request.method == "GET":
+            # busque o id do perfil
+            # crie uma variável para guardar esse perfil
+
+            person = Profile.objects.get(id=kwargs["pk"])
+
+            return render(
+                request,
+                "profile_detail.html",
+                {
+                    "name": person.name,
+                    "github": person.github,
+                    "linkedin": person.linkedin,
+                    "bio": person.bio,
+                },
+            )
+
+        return super().retrieve(request, *args, **kwargs)
+
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
 
